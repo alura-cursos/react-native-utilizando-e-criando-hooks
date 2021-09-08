97f24124323c04723cc7b2b1f7b2381aa931545b
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2021-09-08 11:44:08 -0300

    Aula 2.4

--- src/telas/Home/componentes/Topo.js
+++ src/telas/Home/componentes/Topo.js
@@ -5,9 +5,16 @@ import { carregaTopo } from '../../../servicos/carregaDados';
 import logo from '../../../assets/logo.png';
 
 class Topo extends React.Component {
+    state = {
+        topo: {
+            boasVindas: '',
+            legenda: '',
+        }
+    }
+
     atualizaTopo() {
         const retorno = carregaTopo();
-        console.log(retorno);
+        this.setState({ topo: retorno });
     }
 
     componentDidMount() {
@@ -17,8 +24,8 @@ class Topo extends React.Component {
     render() {
         return <View style={estilos.topo}>
             <Image source={logo} style={estilos.imagem} />
-            <Text style={estilos.boasVindas}>Olá Natalia</Text>
-            <Text style={estilos.legenda}>Encontre os melhores produtores</Text>
+            <Text style={estilos.boasVindas}>{ this.state.topo.boasVindas }</Text>
+            <Text style={estilos.legenda}>{ this.state.topo.legenda }</Text>
         </View>
     }
 }
