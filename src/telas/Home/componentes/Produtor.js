57b765c5ecc7d9fb495b65d6a4370e0eca91ce44
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2021-10-15 11:44:26 -0300

    Aula 4.2

--- src/telas/Home/componentes/Produtor.js
+++ src/telas/Home/componentes/Produtor.js
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, TouchableOpacity, Text, Image, StyleSheet } from 'react-native';
 
 import Estrelas from '../../../componentes/Estrelas';
 
 export default function Produtor({ nome, imagem, distancia, estrelas }) {
-    const [ selecionado, setSelecionado ] = useState(false);
+    const [selecionado, inverterSelecionado] = useReducer(
+        (selecionado) => !selecionado,
+        false
+    );
 
     return <TouchableOpacity 
             style={estilos.cartao}
-            onPress={() => setSelecionado(!selecionado)}
+            onPress={inverterSelecionado}
         >
         <Image source={imagem} style={estilos.imagem} accessibilityLabel={nome} />
         <View style={estilos.informacoes}>
