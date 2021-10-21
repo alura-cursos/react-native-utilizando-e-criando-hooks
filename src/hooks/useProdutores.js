cfcef9e81d6a05ccdb6d35b335a8bbaf263607bc
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2021-10-21 19:00:56 -0300

    Desafio de ordenação

--- src/hooks/useProdutores.js
+++ src/hooks/useProdutores.js
@@ -8,6 +8,9 @@ export default function useProdutores() {
 
     useEffect(() => {
         const retorno = carregaProdutores();
+        retorno.lista.sort(
+            (produtor1, produtor2) => produtor1.distancia - produtor2.distancia,
+        );
         setTitulo(retorno.titulo);
         setLista(retorno.lista);
     }, []);
